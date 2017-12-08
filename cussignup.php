3d23c232e88c90a90b8756c5e843b2053cd39487
Ishmam Islam <32966875+ishmam999@users.noreply.github.com>
2017-12-09 02:49:20 +0600

    login registration
    login and registration back end done

--- cussignup.php
+++ cussignup.php
@@ -55,36 +55,66 @@
 		
 		
 		<div class="login1"> 
-			<form class="col-md-4 col-sm-offset-4 text-center" style="margin: 2%;background-color: #e3e8ef;border: 1px #e3e8ef;border-radius: 5%; opacity:0.6;filter: alpha(opacity=60);text-align: center;margin-left: 35%;padding-top: 2%;padding-bottom: 2%;box-shadow: 5px 10px #989ba0;margin-top: 4%">
+			<form class="col-md-4 col-sm-offset-4 text-center" style="margin: 2%;background-color: #e3e8ef;border: 1px #e3e8ef;border-radius: 5%; opacity:0.6;filter: alpha(opacity=60);text-align: center;margin-left: 35%;padding-top: 2%;padding-bottom: 2%;box-shadow: 5px 10px #989ba0;margin-top: 4%" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
 			  <div class="form-group center">
 				<label for="user" style="font-weight: bold;color: #000000;">Username:</label>
-				<input type="text" class="form-control" id="user" style="width:50%;margin-left: 24%">
+				<input type="text" class="form-control" id="user" name="uname" style="width:50%;margin-left: 24%" required>
 			  </div>
 			  <div class="form-group center">
 				<label for="email" style="font-weight: bold;color: #000000;">Email:</label>
-				<input type="email" class="form-control" id="email" style="width:50%;margin-left: 24%">
+				<input type="email" class="form-control" id="email" name="uemail" style="width:50%;margin-left: 24%" required>
 			  </div>
 			  <div class="form-group">
 				<label for="pwd" style="font-weight: bold;color: #000000;">Password:</label>
-				<input type="password" class="form-control" id="pwd" style="width:50%;margin-left: 24%">
-			  </div>
-			  <div class="form-group">
-				<label for="pwd" style="font-weight: bold;color: #000000;">Re-enter Password:</label>
-				<input type="password" class="form-control" id="pwd" style="width:50%;margin-left: 24%">
+				<input type="password" class="form-control" id="pwd" name="upass" style="width:50%;margin-left: 24%" required>
 			  </div>
 			  <div class="form-group">
 				<label for="mbl" style="font-weight: bold;color: #000000;">Mobile:</label>
-				<input type="text" class="form-control" id="mbl" style="width:50%;margin-left: 24%">
+				<input type="text" class="form-control" id="mbl" name="umobile" style="width:50%;margin-left: 24%" required>
 			  </div>
 			  <div class="form-group">
 				<label for="adrs" style="font-weight: bold;color: #000000;">Address:</label>
-				<input type="text" class="form-control" id="adrs" style="width:50%;margin-left: 24%">
+				<input type="text" class="form-control" id="adrs" name="uaddress" style="width:50%;margin-left: 24%" required>
 			  </div>
 			  
 			  <button type="submit" class="btn btn-default">Sign Up!</button>
+                <br>
+                <br>
+                <a href="index.php">Go back to Login!</a>
 			</form> 
 			
-			
+			<?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                $cuName=$_POST["uname"];
+	            $cuPassword=$_POST["upass"];
+	            $cuEmail=$_POST["uemail"];
+	            $cuMobile=$_POST["umobile"];
+	            $cuAddress=$_POST["uaddress"];
+	            $conn=mysqli_connect("localhost","root","","medicineguide");
+	               if(!$conn){
+		              die("Connection failed: ".mysqli_connect_error);
+	               }
+	               else
+		              //echo "Connected successfully <br>";
+	
+	           $sql="insert into customerlogin(cuUserName,cuPassword) values('".$cuName."', '".$cuPassword."')";
+               $sql1="insert into customer(cuEmail,cuMobile,cuAddress,cuName,cuId) values('".$cuEmail."', '".$cuMobile."','".$cuAddress."', '".$cuName."', LAST_INSERT_ID())";
+	           $result= mysqli_query($conn,$sql)or die(mysqli_error($conn));
+               $result1= mysqli_query($conn,$sql1)or die(mysqli_error($conn));
+	                   if($result && $result1){
+		                  
+                           $message = "Successfully registered!";
+                            echo "<script type='text/javascript'>alert('$message');</script>";
+                           //header("Location:index.php");
+	                   }
+	                   else
+	                   {
+                          
+                           $message = "Registration Unsuccessful!";
+                            echo "<script type='text/javascript'>alert('$message');</script>";
+	                   }
+            }
+            ?>
 		</div>
 		
 		<div class="signup col-sm-offset-8 col-md-8"> 
